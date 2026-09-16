f3617da3057909af77819092983c838eb4046b8a
BetterLordWilliam <willjordan05@icloud.com>
2026-09-16 15:03:42 -0700

    WO: reading into & freeing a buffer is handled correctly now (so we read from stdin for 256 bytes - 1, inject \0 at the last read byte). Also, free the buffer once it is no longer in use.

--- event-loop/my_event_loop.c
+++ event-loop/my_event_loop.c
@@ -9,13 +9,6 @@
 #define EXIT_COMMAND    exit
 
 
-int checkExit(const char* buf, size_t len)
-{
-    for (int i = 0; i < len; i++) {
-        
-    }
-}
-
 int main() 
 {
     int pDone;
@@ -27,49 +20,50 @@ int main()
     
     pDone = 0; 
     printf("PROGRAM STARTED\n");
-    fflush(stdout);
     for (;;) {
         if (pDone) break;
 
         int pollRes = poll(&s, 1, POLL_TIMEOUT);
 
         if (pollRes > 0) {
-            char* buf = (char*)calloc(256, 1);
+            char* buf = (char*)calloc(BUF_SIZE, sizeof(char));
             int by;
             
-            by = read(s.fd, buf, 256 - 1);
+            // read up to a maximum of BUF_SIZE - 1
+            // so that \0 can be injected later for safe printing            
+            by = read(s.fd, buf, BUF_SIZE - 1);
+
+            printf("read return: %d\n", by);
 
             // handle errors
             if (by < 0) {
                 printf("READ FAILED");
+                free(buf); // error occured but we still need to free this
                 return 1;
-            }
 
-            if (by > 0) {
-                // end of input
-                // printf("READ NOTHING")
-            } else {
-                // end of input
-                // printf("READ NORMAL")
+            // read was successful, in sofar as it read something
+            } else if (by > 0) {
+                // printf("%c %c %c %c\n", buf[0], buf[1], buf[2], buf[3]);
             
                 // check that the input buffer begins with sequence 'exit'
+                // set the program done, or `pDone` flag
                 if (    buf[0] == 'e' 
                         && buf[1] == 'x'
                         && buf[2] == 'i'
                         && buf[3] == 't'
-                ) {
-                    printf("begins w/ exit\n"); pDone = 1;
-                } else {
-                    printf("does not begin w/ exit\n");
-                }
+                )
+                    pDone = 1;
 
                 // set the last thing to null terminator
                 // safe to log with `printf`
                 buf[by] = '\0';
+                printf("\e[32m[ECHO]\e[0m: %s\n", buf);
+                // printf("\e[32m[ECHO]\e[0m:DUMMY\n");
+            } else {
+                // read returned 0 bytes read
             }
 
-            printf("\e[32m[ECHO]\e[0m: %s\n", buf);
-            // printf("\e[32m[ECHO]\e[0m:DUMMY\n");
+            free(buf); // free buffer
 
         } else if (pollRes < 0) {
             printf("\e[30m[ERROR]\e[0mthere was an error polling\n");
